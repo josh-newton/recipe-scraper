10bd5c6040a849c236fd4fa464161058f83dda73
Josh Newton <josh.newton@learnsci.co.uk>
2017-03-01 09:31:48 +0000

    Refactoring, remove structure files

--- scraper.py
+++ scraper.py
@@ -18,18 +18,13 @@ open(os.getcwd() + '/recipes.xml', 'w').close()
 jsonFile = open(os.getcwd() + '/recipes.json', 'a')
 xmlFile = open(os.getcwd() + '/recipes.xml', 'a')
 
-# create images directory/remove images contents at start
-if os.path.exists(os.getcwd() + '/images/'):
-	filelist = glob.glob(os.getcwd() + "/images/*")
-	for f in filelist:
-		os.remove(f)
-else:
-    os.makedirs(os.getcwd() + '/images/')
-
-baseUrl = 'https://www.bbc.co.uk/food/recipes/search?page='
-allRecipes = '&cuisines[0]=african&cuisines[1]=american&cuisines[2]=british&cuisines[3]=caribbean&cuisines[4]=chinese&cuisines[5]=east_european&cuisines[6]=french&cuisines[7]=greek&cuisines[8]=indian&cuisines[9]=irish&cuisines[10]=italian&cuisines[11]=japanese&cuisines[12]=mexican&cuisines[13]=nordic&cuisines[14]=north_african&cuisines[15]=portuguese&cuisines[16]=south_american&cuisines[17]=spanish&cuisines[18]=thai_and_south-east_asian&cuisines[19]=turkish_and_middle_eastern'
+bbcBaseUrl = 'https://www.bbc.co.uk'
+baseSearchUrl = 'https://www.bbc.co.uk/food/recipes/search?page='
 titleError = 'BBC - Food - Recipe finder : No results'
-testCuisine = '&cuisines[0]=african'
+
+courseBase = 'courses[0]='
+cuisineBase = 'cuisines[0]='
+dietsBase = 'diets[0]='
 
 courses = [
 	'afternoon_tea',
@@ -78,7 +73,7 @@ diets = [
 ]
 
 selectors = {
-	'link': 'div#article-list h3 a',
+	'recipeLink': 'div#article-list h3 a',
 	'title': 'h1.content-title__text',
 	'image': 'img.recipe-media__image',
 	'description': 'p.recipe-description__text',
@@ -90,22 +85,96 @@ selectors = {
 	'serves': 'p.recipe-metadata__serving'
 }
 
-recipeLinkSelector = 'div#article-list h3 a'
-recipeTitleSelector = 'h1.content-title__text'
-recipeImageSelector = 'img.recipe-media__image'
-recipeDescriptionSelector = 'p.recipe-description__text'
-recipeMethodSelector = 'ol.recipe-method__list li p'
-recipeIngredientsSelector = '.recipe-ingredients__list li'
-recipeChefSelector = 'div.chef__name .chef__link'
-recipePrepTimeSelector = 'p.recipe-metadata__prep-time'
-recipeCookTimeSelector = 'p.recipe-metadata__cook-time'
-recipeServesSelector = 'p.recipe-metadata__serving'
+def saveRecipeDetails(url, cuisine):
+	page = browser.open(url)
+	html = page.read()
+	soup = BeautifulSoup(html, 'lxml')
+
+	title = soup.select(selectors['title'])[0].text
+	print 'Grabbing recipe: ' + title
+
+	try:
+		# Strip removes newline and whitespace characters
+		description = soup.select(selectors['description'])[0].text.strip()
+	except:
+		description = ''
+
+	ingredients = soup.select(selectors['ingredients'])
+	# Grab whole ingredient text with the amount of ingredient ie 400ml water
+	ingredientText = []
+	# Isolate just the ingredient name from the ingredient text
+	ingredientItem = []
+	for ingredient in ingredients:
+		ingredientText.append(ingredient.text.strip())
+		# Attempt to isolate ingredient name
+		if ingredient.select('a'):
+			if ingredient.select('a')[0]:
+				ingredientItem.append(ingredient.select('a')[0].text)
+		else:
+			# if not possible just keep ingredient text
+			ingredientItem.append(ingredient.text.strip())
+	# Remove duplicates
+	ingredientItem = list(set(ingredientItem))
+
+	# Grab cooking method
+	methodList = soup.select(selectors['method'])
+	methodText = []
+	for method in methodList:
+		methodText.append(method.text.strip())
+
+	try:
+		imageTag = soup.select(selectors['image'])
+		if imageTag[0].has_attr('src'):
+			imageSrc = imageTag[0]['src']
+			# imageSrc includes bbc href, we just want to take the image filename
+			filename = imageSrc.split('/')[-1]
+			cwd = os.getcwd()
+			downloadedImgLoc = 'images/' + filename
+			# Download image to images directory
+        	urllib.urlretrieve(imageSrc, cwd + '/images/' + filename)
+	except:
+		downloadedImgLoc = ''
+
+	try:
+		chefName = soup.select(selectors['chef'])[0].text.strip()
+	except:
+		chefName = ''
+
+	try:
+		prepTime = soup.select(selectors['prepTime'])[0].text.strip()
+	except:
+		prepTime = ''
+
+	try:
+		cookTime = soup.select(selectors['cookTime'])[0].text.strip()
+	except:
+		cookTime = ''
+
+	try:
+		serves = soup.select(selectors['serves'])[0].text.strip()
+	except:
+		serves = ''
 
 
-def stitchUrl(page):
-	return baseUrl + page + allRecipes;
-	# return baseUrl + page + testCuisine;
+	ordered = OrderedDict([("title", title), ("description", description), ("cuisine", cuisine), ("image", downloadedImgLoc), ("sourceUrl", url), ("chefName", chefName), ("preparationTime", prepTime), ("cookingTime", cookTime), ("serves", serves), ("ingredientsDesc", ingredientText), ("ingredients", ingredientItem), ("method", methodText)])
+	appendToJsonFile(ordered)
+	appendToXmlFile(ordered)
 
+def appendToJsonFile(dictionary):
+	jsonFile.write(json.dumps(dictionary, sort_keys=False, indent=4, separators=(',', ': ')) + ',\n')
+
+def appendToXmlFile(dictionary):
+	dictXml = dicttoxml.dicttoxml(dictionary, custom_root='recipe', attr_type=False)
+	xmlString = xml.dom.minidom.parseString(dictXml)
+	prettyXml = xmlString.toprettyxml()
+	# Fix utf-8 encoding error, remove xml verion tag
+	prettyXml = prettyXml.encode('utf-8')[23:]
+	xmlFile.write(prettyXml)
+
+def stitchUrl(pageNum, categoryBase, catergoryId):
+	return baseSearchUrl + pageNum + '&' + categoryBase + catergoryId
+
+# Is there a better way of detecting this. BBC does not throw 404 error. Could check for h3.error has string 'No Results Found'
 def pageExists(url):
     page = browser.open(url)
     title = browser.title()
@@ -114,123 +183,61 @@ def pageExists(url):
     else:
         return True
 
-def loopOverPages():
-    currPage = 1;
-    url = stitchUrl(str(currPage))
-    while pageExists(url):
-        recipeUrls = grabRecipeLinksFromListPage(url)
-        for recipe in recipeUrls:
-            if recipe.has_attr('href'):
-                grabRecipeDetails('https://www.bbc.co.uk' + recipe['href'])
-
-        # Update for next loop
-        currPage += 1
-        url = stitchUrl(str(currPage))
-
-def grabRecipeLinksFromListPage(url):
+def grabRecipeLinksFromPage(url):
     page = browser.open(url)
     html = page.read()
     soup = BeautifulSoup(html, 'lxml')
-    recipeLinks = soup.select(recipeLinkSelector)
+    recipeLinks = soup.select(selectors['recipeLink'])
     return recipeLinks
 
-def grabRecipeDetails(url):
-	page = browser.open(url)
-	html = page.read()
-	soup = BeautifulSoup(html, 'lxml')
+def saveRecipesInCategory(categoryBase, categoryId):
+	# Start on first page
+	currPage = 1
+	# Get url
+	url = stitchUrl(str(currPage), categoryBase, categoryId)
+	# While the page exists
+	while pageExists(url):
+		# Get all recipe links from the new page
+		newLinks = grabRecipeLinksFromPage(url)
+		# add to url list
+		for link in newLinks:
+			if link.has_attr('href'):
+				# Go to the recipe page and grab its details
+				saveRecipeDetails(bbcBaseUrl + link['href'], categoryId)
 
-	title = soup.select(recipeTitleSelector)[0].text
-	print 'Grabbing recipe: ' + title
+		# Update for next loop
+		currPage += 1
+		url = stitchUrl(str(currPage), categoryBase, categoryId)
 
-	try:
-		# Strip removes newline and whitespace characters
-		description = soup.select(recipeDescriptionSelector)[0].text.strip()
-	except:
-		description = ''
+def saveRecipesByCuisine():
+	for cuisine in cuisines:
+		saveRecipesInCategory(cuisineBase, cuisine)
 
-	try:
-		ingredients = soup.select(recipeIngredientsSelector)
-		ingredientsDescription = []
-		ingredientsText = []
-		for ingredient in ingredients:
-			ingredientsDescription.append(ingredient.text.strip())
-			if ingredient.select('a')[0]:
-				if ingredient.select('a')[0]:
-					ingredientsText.append(ingredient.select('a')[0].text)
-			else:
-				ingredientsText.append(ingredient.text)
-		# Remove duplicates
-		ingredientsText = list(set(ingredientsText))
-	except:
-		ingredientsText = []
-		ingredientsDescription = []
-
-	try:
-		methodList = soup.select(recipeMethodSelector)
-		methodText = []
-		for method in methodList:
-			methodText.append(method.text)
-	except:
-		methodText = []
-
-	try:
-		imageTag = soup.select(recipeImageSelector)
-		if imageTag[0].has_attr('src'):
-			imageSrc = imageTag[0]['src']
-			filename = imageSrc.split('/')[-1]
-			cwd = os.getcwd()
-			imgFilename = 'images/' + filename
-        	urllib.urlretrieve(imageSrc, cwd + '/images/' + filename)
-	except:
-		imgFilename = ''
-
-	try:
-		chefName = soup.select(recipeChefSelector)[0].text
-	except:
-		chefName = ''
-
-	try:
-		prepTime = soup.select(recipePrepTimeSelector)[0].text
-	except:
-		prepTime = ''
-
-	try:
-		cookTime = soup.select(recipeCookTimeSelector)[0].text
-	except:
-		cookTime = ''
-
-	try:
-		serves = soup.select(recipeServesSelector)[0].text
-	except:
-		serves = ''
-
-	ordered = OrderedDict([("title", title), ("description", description), ("image", imgFilename), ("sourceUrl", url), ("chefName", chefName), ("preparationTime", prepTime), ("cookingTime", cookTime), ("serves", serves), ("ingredientsDesc", ingredientsDescription), ("ingredients", ingredientsText), ("method", methodText)])
-	jsonFile.write(json.dumps(ordered, sort_keys=False, indent=4, separators=(',', ': ')) + ',\n')
-	dictXml = dicttoxml.dicttoxml(ordered, custom_root='recipe', attr_type=False)
-	xmlString = xml.dom.minidom.parseString(dictXml)
-	prettyXml = xmlString.toprettyxml()
-	# Fix utf-8 encoding error, remove xml verion tag
-	prettyXml = prettyXml.encode('utf-8')[23:]
-	xmlFile.write(prettyXml)
-	print ''
 
 def main():
+	# create images directory/remove images contents at start
+	if os.path.exists(os.getcwd() + '/images/'):
+		filelist = glob.glob(os.getcwd() + "/images/*")
+		for f in filelist:
+			os.remove(f)
+	else:
+	    os.makedirs(os.getcwd() + '/images/')
+
 	jsonFile.write('[\n')
-	loopOverPages()
+	saveRecipesByCuisine()
 	jsonFile.write(']')
 
-	# Loop over all cuisines
+	# Loop over all cuisines - cusines[0]=x
 		# Go to a cuisine page
 		# Get recipe links from page
 			# Go to recipe page and grab details
-			# Add recipe details to json
-	# Loop over all course pages
+			# Add recipe details to json and xml
+	# Loop over all course pages - courses[0]=x
+		# Get recipe url, match to an already downloaded recipe, add course to details
+		# If no recipe match, go to recipe page and grab details
+	# Loop over all diet pages - diets[0]=x
 		# Get recipe title, match to an already downloaded recipe, add course to details
 		# If no recipe match, go to recipe page and grab details
-	# Loop over all diet pages
-		# Get recipe title, match to an already downloaded recipe, add course to details
-		# If no recipe match, go to recipe page and grab details
-
-	# Read json and rewrite to xml as second format
+		
 if __name__ == '__main__':
 	main()
